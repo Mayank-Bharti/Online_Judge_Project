ee42f7b959de94ced966e6e1bfd6709e0a68df7c
mayank_bharti <mayankbharti349@gmail.com>
2024-08-09 20:35:33 +0530

    changes

--- public/Client/src/pages/problem_detail.jsx
+++ public/Client/src/pages/problem_detail.jsx
@@ -76,7 +76,7 @@ function ProblemDetail() {
     setError('');
     setTestResults([]);
     try {
-      const response = await fetch('http://localhost:5000/api/submit-code', {
+      const response = await fetch('https://online-judge-project-backend.onrender.com/api/submit-code', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
